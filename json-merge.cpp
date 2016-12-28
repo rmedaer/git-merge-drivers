a521ac053e22e86fbd4455e90ec375618b5690a0
Raphael Medaer (Escaux) <rme@escaux.com>
2016-12-28 13:03:56 +0100

    Rewrote json-merge to do 3-way merge.

--- json-merge.cpp
+++ json-merge.cpp
@@ -1,138 +1,90 @@
 #include <iostream>
 #include <fstream>
 #include <json.hpp>
-#include <getopt.h>
 
 using json = nlohmann::json;
 using namespace std;
 
-enum MERGE_STRATEGY {
-	THEIRS,
-	OURS
-};
-
 void
-merge_json_objects(
-	MERGE_STRATEGY strategy,
-	json *init_json,
-	json *merge_json)
-{
-	// If initial type is different then merge
-	// apply strategy
-	if (init_json->type() != merge_json->type()
-	    || init_json->type() == json::value_t::null
-	    || init_json->type() == json::value_t::boolean
-	    || init_json->type() == json::value_t::number_integer
-	    || init_json->type() == json::value_t::number_float
-	    || init_json->type() == json::value_t::string)
-	{
-		if (strategy == MERGE_STRATEGY::THEIRS)
-		{
-			*init_json = json(*merge_json);
-		}
-		return;
-	}
-
-	if (init_json->type() == json::value_t::object)
-	{
-		for (json::iterator it = merge_json->begin(); it != merge_json->end(); ++it)
-		{
-			auto item = init_json->find(it.key());
-
-			// If corresponding key found in initial object, merge it !
-			// Otherwise insert new key
-			if (item != init_json->end())
-			{
-				merge_json_objects(strategy, &item.value(), &it.value());
-			}
-			else
-			{
-				(*init_json)[it.key()] = it.value();
-			}
-		}
-	}
-	else if (init_json->type() == json::value_t::array)
-	{
-		for (json::iterator it = merge_json->begin(); it != merge_json->end(); ++it)
-		{
-			init_json->push_back(it.value());
-		}
-	}
-}
-
-void
-merge_json_streams(
-	MERGE_STRATEGY strategy,
-	istream& init_stream,
-	istream& merge_stream,
+merge(
+	istream& our_stream,
+	istream& older_stream,
+	istream& their_stream,
 	ostream& dst_stream)
 {
-	auto init_json = json::parse(init_stream);
-	auto merge_json = json::parse(merge_stream);
+	auto our = json::parse(our_stream);
+	auto older = json::parse(older_stream);
+	auto their = json::parse(their_stream);
 
-	merge_json_objects(strategy, &init_json, &merge_json);
-
-	dst_stream << init_json.dump(4);
+	dst_stream << our.patch(json::diff(older, their)).dump(4);
 }
 
-static struct option long_options[] =
+void
+usage(char *program)
 {
-	{ "strategy", required_argument, 0, 's' }
-};
+	cerr << "Usage: " << program << " <current> <older> <other>" << endl;
+}
 
 int main(
 	int argc,
 	char *argv[])
 {
-	MERGE_STRATEGY strategy = MERGE_STRATEGY::THEIRS;
-	char c = 0;
-	while (c != -1)
+	// Validate number of arguments or show usage.
+	if (argc < 4)
 	{
-		int option_index = 0;
-		c = getopt_long(argc, argv, "s:", long_options, &option_index);
+		cerr << "Too few arguments" << endl;
+		usage(argv[0]);
+		exit(1); 
+	}
 
-		switch (c)
-		{
-			case 's':
-				if (strcasecmp(optarg, "theirs") == 0)
-				{
-					strategy = MERGE_STRATEGY::THEIRS;
-				}
-				else if (strcasecmp(optarg, "ours") == 0)
-				{
-					strategy = MERGE_STRATEGY::OURS;
-				}
-				else
-				{
-					cerr << "Unknown strategy " << optarg << endl;
-					abort();
-				}
-				break;
-			case -1:
-				break;
-			default:
-				abort();
-		}
+	// 3 input streams, 1 buffer and 1 output stream
+	ifstream our_stream;
+	ifstream older_stream;
+	ifstream their_stream;
+	ofstream output_stream;
+	ostringstream buffer_stream;
+
+	buffer_stream = ostringstream();
+
+	our_stream.open(argv[1]);
+	if (!our_stream.is_open())
+	{
+		cerr << "Cannot open current file " << argv[1] << endl;
+		exit(1);
+	}
+
+	older_stream.open(argv[2], fstream::in);
+	if (!older_stream.is_open())
+	{
+		cerr << "Cannot open older file " << argv[2] << endl;
+		exit(1);
+	}
+
+	their_stream.open(argv[3], fstream::in);
+	if (!their_stream.is_open())
+	{
+		cerr << "Cannot open other file " << argv[3] << endl;
+		exit(1);
 	}
 	
-	assert(argc - optind >= 2);
+	// Parse and merge JSON streams
+	merge(our_stream, older_stream, their_stream, buffer_stream);
 
-	auto ancestor_stream = ifstream(argv[optind], fstream::in);
-	auto merge_stream = ifstream(argv[optind + 1], fstream::in);
-	auto buffer_stream = ostringstream();
-	ofstream output_stream;
+	// Close input streams
+	our_stream.close();
+	older_stream.close();
+	their_stream.close();
 
-	assert(ancestor_stream);
-	assert(merge_stream);
-
-	merge_json_streams(strategy, ancestor_stream, merge_stream, buffer_stream);
-
-	output_stream.open(argv[optind], ofstream::out);
-	assert(output_stream.is_open());
+	// (Re)open our stream in output and dump buffer
+	output_stream.open(argv[1], ofstream::out);
+	if (!output_stream.is_open())
+	{
+		cerr << "Cannot open output file " << argv[1] << endl;
+		exit(1);
+	} 
 	output_stream << buffer_stream.str();
 
-	ancestor_stream.close();
-	merge_stream.close();
+	// Close output stream
 	output_stream.close();
 
 	return 0;
